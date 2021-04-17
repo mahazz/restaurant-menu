4bfaf6711c621547c81594651cf8a0b0058dabf7
Maha <mahmalzahrany@gmail.com>
2021-04-17 08:15:40 +0300

    Add hamburger navigation menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,20 +11,6 @@
 
     <span class="mobile-btn" @click="openNav()">&#9776;</span>
   </div>
-  <!-- <header>
-    <img alt="Vue logo" src="../assets/logo.png" height="50" />
-    <p>My Vue.js Application</p>
-    <nav>
-      <ul>
-        <li>
-          <router-link to="/">Home</router-link>
-        </li>
-        <li>
-          <router-link to="/about">About</router-link>
-        </li>
-      </ul>
-    </nav>
-  </header>-->
 </template>
 
 <script>
